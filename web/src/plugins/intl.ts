8532085049b550ab6c94ac592fa197c9eea62ff3
Acho Arnold <arnold@ndolestudio.com>
2022-02-26 16:48:56 +0200

    Fix currency fetcher

--- web/src/plugins/intl.ts
+++ web/src/plugins/intl.ts
@@ -1,9 +1,14 @@
 export const DEFAULT_CURRENCY = "USD";
 
 export const getDefaultCurrency = async (): Promise<string> => {
-  const response = await fetch("https://ipapi.co/currency");
-  if (!response.ok) {
+  try {
+    const response = await fetch("https://ipapi.co/currency");
+    if (!response.ok) {
+      return DEFAULT_CURRENCY;
+    }
+    return response.text();
+  } catch (e) {
+    console.error(e);
     return DEFAULT_CURRENCY;
   }
-  return response.text();
 };
