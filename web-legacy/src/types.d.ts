2156a36e2ceb0e20217d88b501281b842a116d91
Acho Arnold Ewin <arnold@ndolestudio.com>
2026-07-05 11:32:07 +0300

    Remove the web-legacy project

--- web-legacy/src/types.d.ts
+++
@@ -1,9 +0,0 @@
-import { CONSTANTS } from "@/plugins/constants";
-import Vuetify from "@/plugins/vuetify";
-
-declare module "vue/types/vue" {
-  interface Vue {
-    $constants: typeof CONSTANTS;
-    $vuetify: typeof Vuetify;
-  }
-}
